17f19c2c563c9dcb089f74926800bf065c05922f
lukas <lukas.luetolf@gmail.com>
2022-08-05 10:43:47 +0200

    Update structure of PayableModule to be inline with Field.

--- src/app/payable/payable.module.ts
+++ src/app/payable/payable.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {PayableComponent} from "./payable.component";
-import {EditPayableDialogComponent} from "./edit-payable-dialog/edit-payable-dialog.component";
-import {CreatePayableDialogComponent} from "./create-payable-dialog/create-payable-dialog.component";
-import {DeletePayableDialogComponent} from "./delete-payable-dialog/delete-payable-dialog.component";
+import {EditPayableDialogComponent} from "./payable-list/edit-payable-dialog/edit-payable-dialog.component";
+import {DeletePayableDialogComponent} from "./payable-list/delete-payable-dialog/delete-payable-dialog.component";
 import {MaterialModule} from "../material/material.module";
 import {HttpClientModule} from "@angular/common/http";
 import { PayableListComponent } from './payable-list/payable-list.component';
-import { PayableDateSelectorComponent } from './payable-date-selector/payable-date-selector.component';
 
 
 
@@ -15,10 +12,8 @@ import { PayableDateSelectorComponent } from './payable-date-selector/payable-da
   declarations: [
     PayableComponent,
     EditPayableDialogComponent,
-    CreatePayableDialogComponent,
     DeletePayableDialogComponent,
     PayableListComponent,
-    PayableDateSelectorComponent,
   ],
   imports: [
     MaterialModule,
