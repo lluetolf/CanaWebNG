6768d55b1e36ac7b566565db0ec4afdd6a83cf33
lukas <lukas.luetolf@gmail.com>
2022-11-15 16:29:17 +0100

    fix api base uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 export const environment = {
   production: true,
   useEmulators: false,
-  apiBaseUri: "https://api.canaweb.app",
+  apiBaseUri: "https://api.canaweb.app/api",
   firebase: {
     apiKey: "AIzaSyDxNB0QvG8UKQDnc1s7ik7oqsg-0CdW758",
     authDomain: "dev-canaweb-firestore.firebaseapp.com",
